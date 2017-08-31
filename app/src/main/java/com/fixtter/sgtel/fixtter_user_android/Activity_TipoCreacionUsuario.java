9909e251934c9e7ebdc936548015e2c7910dd6b9
Giancarlo <danycalderonp@hotmail.com>
2017-08-31 13:11:50 -0500

    Inicio y llamadas

---
+++ app/src/main/java/com/fixtter/sgtel/fixtter_user_android/Activity_TipoCreacionUsuario.java
@@ -0,0 +1,13 @@
+package com.fixtter.sgtel.fixtter_user_android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_TipoCreacionUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__tipo_creacion_usuario);
+    }
+}
--- app/src/main/java/com/fixtter/sgtel/fixtter_user_android/Inicio.java
+++ app/src/main/java/com/fixtter/sgtel/fixtter_user_android/Inicio.java
@@ -1,13 +1,30 @@
 package com.fixtter.sgtel.fixtter_user_android;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Inicio extends AppCompatActivity {
 
+    Button btn_llamada;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+        btn_llamada = (Button) findViewById(R.id.btn_llamada);
+        listeners();
+    }
+    public void listeners(){
+        btn_llamada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String phone = "+51993936066";
+                Intent intent = new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phone, null));
+                startActivity(intent);
+            }
+        });
     }
 }
